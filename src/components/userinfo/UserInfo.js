323b829c31afbc9a55082946e9f52f5d08753e03
bxbdev <bxbdev2022@gmail.com>
2023-03-22 23:41:43 +0800

    updated UserInfo.js

--- src/components/userinfo/UserInfo.js
+++ src/components/userinfo/UserInfo.js
@@ -8,13 +8,23 @@ export const UserInfo = ({user}) => {
     return (
         <div className="user-info">
             <h2>{name}</h2>
-            <div>{phone}</div>
-            <div>
-                <a href={"mailto:" + email}>{email}</a>
-            </div>
-            <div>
-                <a href={"https://" + website} target="_blank" rel="noopener noreferrer">{website}</a>
-            </div>
+            {
+                user ?
+                (
+                    <div className="info">
+                        <div>Phone: {phone}</div>
+                        <div>
+                            Email: <a href={"mailto:" + email}>{email}</a>
+                        </div>
+                        <div>
+                            Website: <a href={"https://" + website} target="_blank" rel="noopener noreferrer">{website}</a>
+                        </div>
+                    </div>
+                )
+                : null
+            }
+                
+            
         </div>
     )
 }
